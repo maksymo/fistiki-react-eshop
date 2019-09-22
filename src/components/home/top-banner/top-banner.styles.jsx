f3574758a1ef59bb47d7d5e638edae92c2164c79
smartdev212 <smartdev212@gmail.com>
2019-09-22 23:37:01 +0300

    feat(top banner): add media queries

--- src/components/home/top-banner/top-banner.styles.jsx
+++ src/components/home/top-banner/top-banner.styles.jsx
@@ -5,12 +5,24 @@ export const TopBannerContainer = styled.section`
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 30em 30em;
   grid-gap: 2em;
-  margin: 2em;
+  @media (max-width: 1400px) {
+    grid-template-rows: 25em 25em;
+  }
+  @media (max-width: 1024px) {
+    grid-template-rows: 20em 20em;
+  }
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 15em 15em;
+  }
 `;
 
 export const BannerLeft = styled.figure`
   grid-column: span 1;
   grid-row: span 2;
+  @media (max-width: 768px) {
+    grid-row: span 1;
+  }
 `;
 
 export const BannerRight = styled.figure`
